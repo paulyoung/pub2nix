f2a31752f462f4bddae93d7537377fffd2e062cc
Paul Young <84700+paulyoung@users.noreply.github.com>
2020-04-29 01:55:04 -0700

    Add binName option for native builds

--- build/native.nix
+++ build/native.nix
@@ -1,13 +1,14 @@
 { pkgs }:
-{ src }:
+{ binName, src }:
 
 let
   build = import ./common.nix { inherit pkgs; } { inherit src; };
 in
   build.overrideAttrs (oldAttrs: {
+    inherit binName;
     name = oldAttrs.name + "-native";
     installPhase = ''
       mkdir -p $out/bin
-      dart2native bin/main.dart -o $out/bin/${oldAttrs.name}
+      dart2native bin/main.dart -o $out/bin/${binName}
     '';
   })
