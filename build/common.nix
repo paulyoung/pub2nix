bfa2c2638728f8e62eb04d31bbd2e9100768d1ea
Paul Young <84700+paulyoung@users.noreply.github.com>
2020-04-28 23:55:41 -0700

    Don't create package_config.json; pub creates this

--- build/common.nix
+++ build/common.nix
@@ -13,79 +13,40 @@ let
   # pubspec.lock as an attribute set. See lock.nix for details.
   lockFile = yaml.readYAML (src + "/pub2nix.lock");
 
-  lockFilePackages =
-    pkgs.lib.optionals (builtins.hasAttr "packages" lockFile) lockFile.packages;
-
-  # ./dart_tool/package_config.json as an attribute set.
-  packageConfig = {
-    configVersion = 2;
-    packages = (pkgs.lib.mapAttrsToList (name: value:
-      let
-        nixStorePath =
-          if builtins.hasAttr "sha256" value
-          then pkgs.fetchzip {
-            inherit (value) sha256;
-            stripRoot = false;
-            url = "${value.description.url}/packages/${name}/versions/${value.version}.tar.gz";
-          }
-          else builtins.throw ''
-            pub2nix: No sha256 hash found for package "${name}". Please run pub2nix-lock.
-          '';
-      in
-        {
-          inherit name;
-          rootUri = "file://${nixStorePath}";
-          packageUri = "lib/";
-        }
-    ) lockFilePackages) ++ [{
-      name = specFile.name;
-      rootUri = "../";
-      packageUri = "lib/";
-    }];
-    generator = "pub2nix";
-    generatorVersion = "0.1.0";
-  };
-
-  packageConfigFile =
-    pkgs.writeText "package_config.json" (builtins.toJSON packageConfig);
-
-  # Apparently pub still expects a .pub-cache directory to exist an be populated
-  # despite .dart_tool/package_config.json already containing paths to packages.
-  #
-  # TODO: determine if we can avoid this altogether.
   # TODO: https://github.com/dart-lang/pub/blob/6deb457048deb435009b36a4cd2d86003d107cf4/lib/src/source/hosted.dart#L441-L468
   pubCache =
     let
-      step = (state: tuple:
+      step = (state: package:
         let
-          lockPackage = tuple.fst;
-          configPackage = tuple.snd;
           pubCachePathParent = pkgs.lib.concatStringsSep "/" [
             "$out"
-            lockPackage.source
-            (pkgs.lib.removePrefix "https://" lockPackage.description.url)
+            package.source
+            (pkgs.lib.removePrefix "https://" package.description.url)
           ];
           pubCachePath = pkgs.lib.concatStringsSep "/" [
             pubCachePathParent
-            "${lockPackage.description.name}-${lockPackage.version}"
+            "${package.description.name}-${package.version}"
           ];
-          nixStorePath = pkgs.lib.removePrefix "file://" configPackage.rootUri;
+          nixStorePath = pkgs.fetchzip {
+            inherit (package) sha256;
+            stripRoot = false;
+            url = pkgs.lib.concatStringsSep "/" [
+              package.description.url
+              "packages"
+              package.description.name
+              "versions"
+              "${package.version}.tar.gz"
+            ];
+          };
         in
           state + ''
             mkdir -p ${pubCachePathParent}
             ln -s ${nixStorePath} ${pubCachePath}
           ''
       );
-      packages =
-        # NOTE: these lists are different lengths because the package config
-        # packages list contains the entry point (the project we are building)
-        # but this should be fine because the entry point is last and will be
-        # dropped.
-        pkgs.lib.zipLists
-          (builtins.attrValues lockFile.packages)
-          packageConfig.packages;
 
-      synthesize = builtins.foldl' step "" packages;
+      synthesize =
+        builtins.foldl' step "" (builtins.attrValues lockFile.packages);
     in
       pkgs.runCommand "${specFile.name}_pub-cache" {} synthesize;
 in
@@ -101,7 +62,6 @@ in
     ];
     preBuildPhases = [
       "dotPackages"
-      "packageConfig"
       "pubCache"
       "pubGet"
       "dartAnalyzer"
@@ -109,11 +69,6 @@ in
     # Some tooling still expects this file to exist
     dotPackages = ''
       touch .packages
-    '';
-    # Use cat to avoid permissions issues with cp and mv
-    packageConfig = ''
-      mkdir -p .dart_tool
-      cat ${packageConfigFile} > .dart_tool/package_config.json
     '';
     pubCache = ''
       ln -s ${pubCache} .pub-cache
