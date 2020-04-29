edd5a8e2f2246a06e292ae61e5ff724163ccb6bb
Paul Young <84700+paulyoung@users.noreply.github.com>
2020-04-29 00:30:18 -0700

    Add example fetchTarball code

--- examples/pub2nix_simple_example/nix/overlays/pub2nix.nix
+++ examples/pub2nix_simple_example/nix/overlays/pub2nix.nix
@@ -1,1 +1,14 @@
 import ../../../..
+
+# This file would typically have contents similar to the following:
+
+# let
+#   owner = "paulyoung";
+#   repo = "pub2nix";
+#   rev = "bfa2c2638728f8e62eb04d31bbd2e9100768d1ea";
+#   sha256 = "0nsn85gwywis9w1fwmml394wh6dldznb3fq5ym7a3lvvc4cg32nz";
+# in
+#   import (builtins.fetchTarball {
+#     inherit sha256;
+#     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+#   })
