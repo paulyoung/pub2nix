4889ed14875458b93135d7f0c27cfa144551ad5e
Paul Young <84700+paulyoung@users.noreply.github.com>
2020-07-04 22:32:05 -0700

    Update pub2nix overlay in example

--- examples/pub2nix_simple_example/nix/overlays/pub2nix.nix
+++ examples/pub2nix_simple_example/nix/overlays/pub2nix.nix
@@ -5,8 +5,8 @@ import ../../../..
 # let
 #   owner = "paulyoung";
 #   repo = "pub2nix";
-#   rev = "3493d89c189713472cbe40bcc8e9a2653775c6ca";
-#   sha256 = "0y6mgjg37gn932m5abim3y0shkmp8yx4rn4wc4a2h439dkd9gfkq";
+#   rev = "2c921a46c10dc72ff33005722e4c642852ff22af";
+#   sha256 = "14lcylqdmigsl7argcx1pprap6abi3znghj7469f5cif50ydw5gk";
 # in
 #   import (builtins.fetchTarball {
 #     inherit sha256;
